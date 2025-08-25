03e0f38b6edf117f421caf1b4ce143121af0ee3d
Shak Ragoler <shak.ragoler@gmail.com>
2025-08-25 00:01:28 -0700

    Expanding the API

--- app.py
+++ app.py
@@ -20,3 +20,18 @@ async def get_styles():
 @app.get("/script.js")
 async def get_script():
     return FileResponse("script.js")
+
+
+@app.post("/add_record")
+async def add_record(name, date, symptoms, hazards):
+    database.add_record(name, date, symptoms, hazards)
+
+
+@app.post("/remove_record")
+async def remove_record(idx):
+    database.remove_record(idx)
+
+
+@app.post("/delete_all_records")
+async def delete_all_records():
+    database.delete_all_records()
